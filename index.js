6032eff8807feb378532d145a4bb9846cd8c8f6e
Pushpendra-Singh <pushpendra.singh@habilelabs.io>
2022-03-06 22:05:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const venom = require('venom-bot');
+const cron = require('node-cron');
+
+
+venom
+    .create({
+        session: 'session-name', //name of session
+        multidevice: false
+    })
+    .then((client) => {
+        cron.schedule('* 21 * * *', () => {
+            start(client)
+        });
+    })
+    .catch((erro) => {
+        console.log(erro);
+    });
+
+function start(client) {
+
+    client
+        .sendText('918958315050@c.us', '👋 Hi, Please login keka and stop webclock.')
+        .then((result) => {
+            console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+        });
+}
